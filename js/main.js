4c6be786d9cec7d3eafe9ea0ae6d8ae923ce5761
Matt Gelfand <matthew.gelfand@gmail.com>
2014-08-11 16:20:01 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#figs').click(function() {
+			$('.sub-nav').toggleClass('visible');
+		});
+
+	$('#pine').click(function() {
+			$('.sub-nav1').toggleClass('visible');
+		});
+	});
